9e32ddf465d80b0f4b63918873ad9f670d6744b7
Mirian Margiani <mirian@margiani.ch>
2018-11-13 01:10:27 +0100

    implement offline caching

---
+++ data/storage.js
@@ -0,0 +1,62 @@
+
+function getDatabase() {
+    return LocalStorage.openDatabaseSync("meteoswiss", "1.0", "MeteoSwiss Offline Cache", 1000000)
+}
+
+function init() {
+    var db = getDatabase();
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS zipcodes(zip INTEGER, name TEXT)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS data(timestamp INTEGER PRIMARY KEY, zip INTEGER, converted TEXT, raw TEXT)');
+    });
+}
+
+function setData(timestamp, zip, converted, raw) {
+    var db = getDatabase();
+    var res = "";
+
+    db.transaction(function(tx) {
+        var rs = tx.executeSql('INSERT OR REPLACE INTO data VALUES (?,?,?,?);', [timestamp, zip, converted, raw]);
+        console.log(rs.rowsAffected)
+
+        if (rs.rowsAffected > 0) {
+            res = "ok";
+        } else {
+            res = "error";
+        }
+    });
+
+    return res; // 'ok' or 'error'
+}
+
+function defaultFor(arg, val) { return typeof arg !== 'undefined' ? arg : val; }
+
+function getData(zip, mostRecent, newerThan) {
+    var db = getDatabase();
+    var res = [];
+
+    newerThan = defaultFor(newerThan, 0)
+    mostRecent = defaultFor(mostRecent, true)
+
+    try {
+        db.transaction(function(tx) {
+            var rs = tx.executeSql('SELECT * FROM data WHERE zip=? AND timestamp>=? ORDER BY timestamp DESC;', [zip, newerThan]);
+
+            for (var i = 0; i < rs.rows.length; i++) {
+                res.push({
+                    zip: rs.rows.item(i).zip,
+                    timestamp: rs.rows.item(i).timestamp,
+                    converted: rs.rows.item(i).converted,
+                    raw: rs.rows.item(i).raw,
+                })
+
+                if (mostRecent) break;
+            }
+        })
+    } catch(e) {
+        console.log("error while loading data: zip=" + zip + " newerThan=" + newerThan)
+        return [];
+    }
+
+    return res
+}
