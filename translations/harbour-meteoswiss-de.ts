4d6a367a6a9b2cde84b96b5b8f8adb523d523809
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2019-01-01 16:49:34 +0100

    add dummy translation files

---
+++ translations/harbour-meteoswiss-de.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="New location">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="Refresh">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ForecastPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
