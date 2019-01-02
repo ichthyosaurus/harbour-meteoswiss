bb05ed51cb1fb43b01e6b0926b5fa3f887207aa6
Mirian Margiani <mirian@margiani.ch>
2019-01-02 15:25:56 +0100

    clean-up storage.js

--- qml/js/storage.js
+++ qml/js/storage.js
@@ -1,4 +1,6 @@
 
+function defaultFor(arg, val) { return typeof arg !== 'undefined' ? arg : val; }
+
 function getDatabase() {
     return LocalStorage.openDatabaseSync("meteoswiss", "1.0", "MeteoSwiss Offline Cache", 1000000)
 }
@@ -9,23 +11,6 @@ function init() {
         tx.executeSql('CREATE TABLE IF NOT EXISTS locations(zip INTEGER PRIMARY KEY, name TEXT, canton TEXT, cantonId TEXT)');
         tx.executeSql('CREATE TABLE IF NOT EXISTS data(timestamp INTEGER, zip INTEGER, converted TEXT, raw TEXT, PRIMARY KEY(timestamp, zip))');
     });
-}
-
-function setData(timestamp, zip, converted, raw) {
-    var db = getDatabase();
-    var res = "";
-
-    db.transaction(function(tx) {
-        var rs = tx.executeSql('INSERT OR REPLACE INTO data VALUES (?,?,?,?);', [timestamp, zip, converted, raw]);
-
-        if (rs.rowsAffected > 0) {
-            res = "ok";
-        } else {
-            res = "error";
-        }
-    });
-
-    return res; // 'ok' or 'error'
 }
 
 function addLocation(zip, name, canton, cantonId) {
@@ -62,38 +47,6 @@ function removeLocation(zip) {
     } catch(e) {
         console.log("error while removing location: zip=" + zip)
         res = "error"
-    }
-
-    return res
-}
-
-function defaultFor(arg, val) { return typeof arg !== 'undefined' ? arg : val; }
-
-function getData(zip, mostRecent, newerThan) {
-    var db = getDatabase();
-    var res = [];
-
-    newerThan = defaultFor(newerThan, 0)
-    mostRecent = defaultFor(mostRecent, true)
-
-    try {
-        db.transaction(function(tx) {
-            var rs = tx.executeSql('SELECT * FROM data WHERE zip=? AND timestamp>=? ORDER BY timestamp DESC;', [zip, newerThan]);
-
-            for (var i = 0; i < rs.rows.length; i++) {
-                res.push({
-                    zip: rs.rows.item(i).zip,
-                    timestamp: rs.rows.item(i).timestamp,
-                    converted: rs.rows.item(i).converted,
-                    raw: rs.rows.item(i).raw,
-                })
-
-                if (mostRecent) break;
-            }
-        })
-    } catch(e) {
-        console.log("error while loading data: zip=" + zip + " newerThan=" + newerThan)
-        return [];
     }
 
     return res
@@ -174,4 +127,51 @@ function getDataSummary(zip) {
         temp: temp,
         rain: rain,
     }
+}
+
+function setData(timestamp, zip, converted, raw) {
+    var db = getDatabase();
+    var res = "";
+
+    db.transaction(function(tx) {
+        var rs = tx.executeSql('INSERT OR REPLACE INTO data VALUES (?,?,?,?);', [timestamp, zip, converted, raw]);
+
+        if (rs.rowsAffected > 0) {
+            res = "ok";
+        } else {
+            res = "error";
+        }
+    });
+
+    return res; // 'ok' or 'error'
+}
+
+function getData(zip, mostRecent, newerThan) {
+    var db = getDatabase();
+    var res = [];
+
+    newerThan = defaultFor(newerThan, 0)
+    mostRecent = defaultFor(mostRecent, true)
+
+    try {
+        db.transaction(function(tx) {
+            var rs = tx.executeSql('SELECT * FROM data WHERE zip=? AND timestamp>=? ORDER BY timestamp DESC;', [zip, newerThan]);
+
+            for (var i = 0; i < rs.rows.length; i++) {
+                res.push({
+                    zip: rs.rows.item(i).zip,
+                    timestamp: rs.rows.item(i).timestamp,
+                    converted: rs.rows.item(i).converted,
+                    raw: rs.rows.item(i).raw,
+                })
+
+                if (mostRecent) break;
+            }
+        })
+    } catch(e) {
+        console.log("error while loading data: zip=" + zip + " newerThan=" + newerThan)
+        return [];
+    }
+
+    return res
 }
