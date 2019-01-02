4309881ce34b52b05d3b79600b61717b404fd8c9
Mirian Margiani <mirian@margiani.ch>
2019-01-02 23:17:39 +0100

    update translation catalogues

--- translations/harbour-meteoswiss-de.ts
+++ translations/harbour-meteoswiss-de.ts
@@ -2,24 +2,115 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name></name>
-    <message id="New location">
-        <source></source>
+    <name>AboutPage</name>
+    <message>
+        <source>About MeteoSwiss</source>
         <translation type="unfinished"></translation>
     </message>
-    <message id="Refresh">
-        <source></source>
+    <message>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright, Federal Office of Meteorology and Climatology MeteoSwiss.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>https://www.meteoschweiz.admin.ch/</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ForecastItem</name>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed...</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ForecastPage</name>
     <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to load data!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocationSearchPage</name>
+    <message>
+        <source>Add Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>New location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TablePage</name>
+    <message>
+        <source>Detailed Forecast</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temp.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Precip.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Description</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
