aed37863d1f5a5e21341a6dddca8f9c2ac1b58da
Mirian Margiani <mirian@margiani.ch>
2019-01-04 14:23:52 +0100

    add some semicolons

--- qml/js/forecast.js
+++ qml/js/forecast.js
@@ -1,19 +1,19 @@
 
 function date_diff(a, b) {
-    var d1 = new Date()
-    var d2 = new Date()
+    var d1 = new Date();
+    var d2 = new Date();
 
-    d1.setTime(a)
-    d2.setTime(b)
+    d1.setTime(a);
+    d2.setTime(b);
 
-    return d1-d2
+    return d1-d2;
 }
 
 function convert_raw(raw) {
-    var data = []
+    var data = [];
 
     raw.sort(function(a, b) {
-        return date_diff(a.max_date, b.max_date)
+        return date_diff(a.max_date, b.max_date);
     })
 
     for (var day = 0; day < raw.length; day++) {
@@ -27,8 +27,8 @@ function convert_raw(raw) {
                     strokeColor: "rgba(234,77,79,1)",
                     pointColor: "rgba(234,77,79,1)",
                     data: [],
-                    symbols: []
-                }]
+                    symbols: [],
+                }],
             },
             rainfall: {
                 labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
@@ -39,71 +39,73 @@ function convert_raw(raw) {
                     data: [],
                     tableData: [],
                 }]
-            }
-        }
+            },
+            // TODO add variance_rain, variance_range, wind
+        };
 
-        var date = new Date()
-        date.setTime(raw[day].max_date)
+        var date = new Date();
+        date.setTime(raw[day].max_date);
 
-        var weekday = date.getDay()
+        var weekday = date.getDay();
 
         if (weekday == 0) {
-            weekday = "So"
+            weekday = "So";
         } else if (weekday == 1) {
-            weekday = "Mo"
+            weekday = "Mo";
         } else if (weekday == 2) {
-            weekday = "Di"
+            weekday = "Di";
         } else if (weekday == 3) {
-            weekday = "Mi"
+            weekday = "Mi";
         } else if (weekday == 4) {
-            weekday = "Do"
+            weekday = "Do";
         } else if (weekday == 5) {
-            weekday = "Fr"
+            weekday = "Fr";
         } else if (weekday == 6) {
-            weekday = "Sa"
+            weekday = "Sa";
         }
 
-        dayData.dateString = weekday + "., " + date.getDate() + ". " + (date.getMonth()+1) + ". " + date.getFullYear()
-        dayData.date = date.toJSON()
+        dayData.dateString = weekday + "., " + date.getDate() + ". " + (date.getMonth()+1) + ". " + date.getFullYear();
+        dayData.date = date.toJSON();
 
         raw[day].rainfall.sort(function(a, b) {
-            return date_diff(a[0], b[0])
-        })
+            return date_diff(a[0], b[0]);
+        });
 
         raw[day].temperature.sort(function(a, b) {
-            return date_diff(a[0], b[0])
-        })
+            return date_diff(a[0], b[0]);
+        });
 
         raw[day].symbols.sort(function(a, b) {
-            return date_diff(a.timestamp, b.timestamp)
-        })
+            return date_diff(a.timestamp, b.timestamp);
+        });
 
         for (var rain = 0; rain < raw[day].rainfall.length; rain++) {
-            dayData.rainfall.datasets[0].data.push(raw[day].rainfall[rain][1])
-            dayData.rainfall.datasets[0].tableData.push(raw[day].rainfall[rain][1])
+            dayData.rainfall.datasets[0].data.push(raw[day].rainfall[rain][1]);
+            dayData.rainfall.datasets[0].tableData.push(raw[day].rainfall[rain][1]);
         }
 
-        var minR = Math.min.apply(Math, dayData.rainfall.datasets[0].data)
-        var maxR = Math.max.apply(Math, dayData.rainfall.datasets[0].data)
+        var minR = Math.min.apply(Math, dayData.rainfall.datasets[0].data);
+        var maxR = Math.max.apply(Math, dayData.rainfall.datasets[0].data);
 
-        if ((minR == maxR) && minR == 0.0) { // WARNING ugly hack: set dummy data to force scale being shown
-            dayData.rainfall.datasets[0].fillColor = "rgba(151,187,205,0.0)"
-            dayData.rainfall.datasets[0].strokeColor = "rgba(151,187,205,0.0)"
-            dayData.rainfall.datasets[0].pointColor = "rgba(151,187,205,0.0)"
-            dayData.rainfall.datasets[0].data[0] = 0.3
+        // @disable-check M126
+        if ((minR === maxR) && minR == 0.0) { // WARNING ugly hack: set dummy data to force scale being shown
+            dayData.rainfall.datasets[0].fillColor = "rgba(151,187,205,0.0)";
+            dayData.rainfall.datasets[0].strokeColor = "rgba(151,187,205,0.0)";
+            dayData.rainfall.datasets[0].pointColor = "rgba(151,187,205,0.0)";
+            dayData.rainfall.datasets[0].data[0] = 0.3;
         }
 
         for (var temp = 0; temp < raw[day].temperature.length; temp++) {
-            dayData.temperature.datasets[0].data.push(raw[day].temperature[temp][1])
-            dayData.temperature.datasets[0].symbols.push(0)
+            dayData.temperature.datasets[0].data.push(raw[day].temperature[temp][1]);
+            dayData.temperature.datasets[0].symbols.push(0);
         }
 
         for (var sym = 0; sym < raw[day].symbols.length; sym++) {
-            dayData.temperature.datasets[0].symbols[(sym*3)+2] = raw[day].symbols[sym].weather_symbol_id
+            dayData.temperature.datasets[0].symbols[(sym*3)+2] = raw[day].symbols[sym].weather_symbol_id;
         }
 
-        dayData.isSane = true
-        data.push(dayData)
+        dayData.isSane = true;
+        data.push(dayData);
     }
 
     return data
@@ -121,7 +123,7 @@ var emptyDummyDay = {
             pointColor: "rgba(234,77,79,1)",
             data: [],
             symbols: []
-        }]
+        }],
     },
     rainfall: {
         labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
@@ -131,11 +133,11 @@ var emptyDummyDay = {
             pointColor: "rgba(151,187,205,1)",
             data: [],
             tableData: [],
-        }]
-    }
-}
+        }],
+    },
+};
 
-var fullData = [emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay]
+var fullData = [emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay];
 
 
 function httpGet(url) {
@@ -146,14 +148,14 @@ function httpGet(url) {
 }
 
 function fallbackToArchive(archived, errorMessage) {
-    console.log("warning: " + errorMessage)
-    fullData = JSON.parse(archived.converted)
+    console.log("warning: " + errorMessage);
+    fullData = JSON.parse(archived.converted);
     WorkerScript.sendMessage({
         'zip': archived.zip,
         'timestamp': archived.timestamp,
         'data': fullData,
         'raw': JSON.parse(archived.raw),
-    })
+    });
 }
 
 // function sleep(ms) { // NOTE for debugging the loading display
@@ -177,7 +179,7 @@ WorkerScript.onMessage = function(message) {
     var archived = null
 
     if (message && message.data) {
-        archived = message.data
+        archived = message.data;
     }
 
     // if (message && message.dummy) {
