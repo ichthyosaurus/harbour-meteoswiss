5642e2f8189bf0528fec15230007ebc026dd7caf
Mirian Margiani <mirian@margiani.ch>
2019-01-26 13:26:26 +0100

    watch qchart dir in liverunner

--- liverunner.sh
+++ liverunner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while sleep_until_modified qml/*.qml qml/**/*.qml qml/**/**/*.qml qml/js/*.*; do
+while sleep_until_modified qml/qchart/*.* qml/*.qml qml/**/*.qml qml/**/**/*.qml qml/js/*.*; do
     killall qml
     qml qml/harbour-meteoswiss.qml &
 done
