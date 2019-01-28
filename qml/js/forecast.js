fe7fa032e9c06483005b43157cce0e8002f2d5a9
Mirian Margiani <mirian@margiani.ch>
2019-01-28 12:28:52 +0100

    convert variance data

--- qml/js/forecast.js
+++ qml/js/forecast.js
@@ -9,114 +9,6 @@ function date_diff(a, b) {
     return d1-d2;
 }
 
-function convert_raw(raw) {
-    var data = [];
-
-    raw.sort(function(a, b) {
-        return date_diff(a.max_date, b.max_date);
-    })
-
-    for (var day = 0; day < raw.length; day++) {
-        var dayData = {
-            date: '',
-            temperature: {
-                labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
-                datasets: [{
-                    fillColor: "rgba(0,0,0,0)",
-                    strokeColor: "rgba(234,77,79,1)",
-                    pointColor: "rgba(234,77,79,1)",
-                    data: [],
-                    symbols: [],
-                }],
-            },
-            rainfall: {
-                labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
-                datasets: [{
-                    fillColor: "rgba(151,187,205,0.5)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    data: [],
-                    tableData: [],
-                }]
-            },
-            wind: {
-                labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
-                datasets: [{
-                    fillColor: "rgba(0,0,0,0)",
-                    strokeColor: "rgba(255,255,0,1)",
-                    pointColor: "rgba(255,255,0,1)",
-                    data: [],
-                    symbols: [],
-                }],
-            },
-            // TODO add variance_rain, variance_range
-        };
-
-        var date = new Date();
-        date.setTime(raw[day].max_date);
-        dayData.date = date.toJSON();
-
-        raw[day].rainfall.sort(function(a, b) {
-            return date_diff(a[0], b[0]);
-        });
-
-        raw[day].temperature.sort(function(a, b) {
-            return date_diff(a[0], b[0]);
-        });
-
-        raw[day].wind.data.sort(function(a, b) {
-            return date_diff(a[0], b[0]);
-        });
-
-        raw[day].wind.symbols.sort(function(a, b) {
-            return date_diff(a.timestamp, b.timestamp);
-        });
-
-        raw[day].symbols.sort(function(a, b) {
-            return date_diff(a.timestamp, b.timestamp);
-        });
-
-        for (var rain = 0; rain < raw[day].rainfall.length; rain++) {
-            dayData.rainfall.datasets[0].data.push(raw[day].rainfall[rain][1]);
-            dayData.rainfall.datasets[0].tableData.push(raw[day].rainfall[rain][1]);
-        }
-
-        var minR = Math.min.apply(Math, dayData.rainfall.datasets[0].data);
-        var maxR = Math.max.apply(Math, dayData.rainfall.datasets[0].data);
-
-        // @disable-check M126
-        if ((minR === maxR) && minR == 0.0) { // WARNING ugly hack: set dummy data to force scale being shown
-            dayData.rainfall.datasets[0].fillColor = "rgba(151,187,205,0.0)";
-            dayData.rainfall.datasets[0].strokeColor = "rgba(151,187,205,0.0)";
-            dayData.rainfall.datasets[0].pointColor = "rgba(151,187,205,0.0)";
-            dayData.rainfall.datasets[0].data[0] = 0.3;
-        }
-
-        for (var temp = 0; temp < raw[day].temperature.length; temp++) {
-            dayData.temperature.datasets[0].data.push(raw[day].temperature[temp][1]);
-            dayData.temperature.datasets[0].symbols.push(0);
-        }
-
-        for (var wind = 0; wind < raw[day].wind.data.length; wind++) {
-            dayData.wind.datasets[0].data.push(raw[day].wind.data[wind][1]);
-            dayData.wind.datasets[0].symbols.push("");
-        }
-
-        for (var wind_sym = 0; wind_sym < raw[day].wind.symbols.length; wind_sym++) {
-            dayData.wind.datasets[0].symbols[wind_sym*2] = raw[day].wind.symbols[wind_sym].symbol_id;
-        }
-
-        for (var sym = 0; sym < raw[day].symbols.length; sym++) {
-            dayData.temperature.datasets[0].symbols[(sym*3)+2] = raw[day].symbols[sym].weather_symbol_id;
-        }
-
-        dayData.isSane = true;
-        data.push(dayData);
-    }
-
-    return data
-}
-
 var emptyDummyDay = {
     isSane: false,
     date: '',
@@ -151,6 +43,139 @@ var emptyDummyDay = {
         }],
     },
 };
+
+function convert_raw(raw) {
+    var data = [];
+
+    raw.sort(function(a, b) {
+        return date_diff(a.max_date, b.max_date);
+    })
+
+    for (var day = 0; day < raw.length; day++) {
+        var dayData = {
+            isSane: false,
+            date: '',
+            temperature: {
+                labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+                datasets: [{ // estimate
+                        fillColor: "rgba(234,77,79,0)",
+                        strokeColor: "rgba(234,77,79,1)",
+                        pointColor: "rgba(234,77,79,1)",
+                        data: [],
+                        symbols: []
+                    },{ // minimum
+                        fillColor: "rgba(234,77,79,0.6)",
+                        strokeColor: "rgba(234,77,79,0.6)",
+                        pointColor: "rgba(234,77,79,0.3)",
+                        data: [],
+                    },{ // maximum
+                        fillColor: "rgba(234,77,79,0.6)",
+                        strokeColor: "rgba(234,77,79,0.6)",
+                        pointColor: "rgba(234,77,79,0.3)",
+                        data: [],
+                    },
+                ],
+            },
+            rainfall: {
+                labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+                datasets: [{ // estimate
+                        fillColor: "rgba(151,187,205,0.5)",
+                        strokeColor: "rgba(151,187,205,1)",
+                        pointColor: "rgba(151,187,205,1)",
+                        data: [],
+                        tableData: [],
+                    },{ // minimum
+                        fillColor: "rgba(151,187,205,0)",
+                        strokeColor: "rgba(151,187,205,0.6)",
+                        pointColor: "rgba(151,187,205,0.3)",
+                        data: [],
+                    },{ // maximum
+                        fillColor: "rgba(151,187,205,0)",
+                        strokeColor: "rgba(151,187,205,0.6)",
+                        pointColor: "rgba(151,187,205,0.3)",
+                        data: [],
+                    },
+                ],
+            },
+            wind: {
+                labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+                datasets: [{
+                    fillColor: "rgba(0,0,0,0)",
+                    strokeColor: "rgba(255,255,0,1)",
+                    pointColor: "rgba(255,255,0,1)",
+                    data: [],
+                    symbols: [],
+                }],
+            },
+            // TODO add variance_rain, variance_range
+        };
+
+        if (   raw[day].rainfall.length != raw[day].temperature.length
+            || raw[day].rainfall.length != raw[day].wind.data.length
+            || raw[day].rainfall.length != raw[day].variance_rain.length
+            || raw[day].rainfall.length != raw[day].variance_range.length
+        ) {
+            console.log("failed converting data for day " + day + ": datasets have different lengths")
+            dayData.isSane = true;
+            data.push(dayData);
+            continue;
+        }
+
+        var date = new Date();
+        date.setTime(raw[day].max_date);
+        dayData.date = date.toJSON();
+
+        // sort data
+        raw[day].rainfall.sort(function(a, b) { return date_diff(a[0], b[0]); });
+        raw[day].variance_rain.sort(function(a, b) { return date_diff(a[0], b[0]); });
+        raw[day].temperature.sort(function(a, b) { return date_diff(a[0], b[0]); });
+        raw[day].variance_range.sort(function(a, b) { return date_diff(a[0], b[0]); });
+        raw[day].wind.data.sort(function(a, b) { return date_diff(a[0], b[0]); });
+        raw[day].wind.symbols.sort(function(a, b) { return date_diff(a.timestamp, b.timestamp); });
+        raw[day].symbols.sort(function(a, b) { return date_diff(a.timestamp, b.timestamp); });
+
+        // convert data
+        for (var hour = 0; hour < raw[day].rainfall.length; hour++) {
+            dayData.rainfall.datasets[0].data.push(raw[day].rainfall[hour][1]);         // estimate
+            dayData.rainfall.datasets[0].tableData.push(raw[day].rainfall[hour][1]);
+            dayData.rainfall.datasets[1].data.push(raw[day].variance_rain[hour][1]);    // minimum
+            dayData.rainfall.datasets[2].data.push(raw[day].variance_rain[hour][2]);    // maximum
+
+            dayData.temperature.datasets[0].data.push(raw[day].temperature[hour][1]);       // estimate
+            dayData.temperature.datasets[0].symbols.push(0);
+            dayData.temperature.datasets[1].data.push(raw[day].variance_range[hour][1]);    // minimum
+            dayData.temperature.datasets[2].data.push(raw[day].variance_range[hour][2]);    // maximum
+
+            dayData.wind.datasets[0].data.push(raw[day].wind.data[hour][1]);
+            dayData.wind.datasets[0].symbols.push("");
+        }
+
+        for (var wind_sym = 0; wind_sym < raw[day].wind.symbols.length; wind_sym++) {
+            dayData.wind.datasets[0].symbols[wind_sym*2] = raw[day].wind.symbols[wind_sym].symbol_id;
+        }
+
+        for (var sym = 0; sym < raw[day].symbols.length; sym++) {
+            dayData.temperature.datasets[0].symbols[(sym*3)+2] = raw[day].symbols[sym].weather_symbol_id;
+        }
+
+        // check if there is any precipitation
+        var minR = Math.min.apply(Math, dayData.rainfall.datasets[0].data);
+        var maxR = Math.max.apply(Math, dayData.rainfall.datasets[0].data);
+
+        // @disable-check M126
+        if ((minR === maxR) && minR == 0.0) { // WARNING ugly hack: set dummy data to force scale being shown
+            dayData.rainfall.datasets[0].fillColor = "rgba(151,187,205,0.0)";
+            dayData.rainfall.datasets[0].strokeColor = "rgba(151,187,205,0.0)";
+            dayData.rainfall.datasets[0].pointColor = "rgba(151,187,205,0.0)";
+            dayData.rainfall.datasets[0].data[0] = 0.3;
+        }
+
+        dayData.isSane = true;
+        data.push(dayData);
+    }
+
+    return data
+}
 
 var fullData = [emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay];
 
