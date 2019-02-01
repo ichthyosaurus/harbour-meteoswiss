9b51ae525877d143a154f810bd22bf16009a915c
Mirian Margiani <mirian@margiani.ch>
2019-02-01 11:02:29 +0100

    add user agent of official app but don't use it

--- qml/js/forecast.js
+++ qml/js/forecast.js
@@ -161,6 +161,7 @@ var fullData = [emptyDummyDay, emptyDummyDay, emptyDummyDay, emptyDummyDay, empt
 
 function httpGet(url) {
     var xmlHttp = new XMLHttpRequest();
+    // xmlHttp.setRequestHeader("User-Agent", "MeteoSwissApp-2.3.3-Android");
     xmlHttp.open("GET", url, false); // false for synchronous request
     xmlHttp.send(null);
     return xmlHttp;
