cb44699449dbeeddd7029da45006f63a2b3bd1a0
Mirian Margiani <mirian@margiani.ch>
2019-09-05 09:51:53 +0200

    Add new translation catalogues for fr and it

---
+++ translations/harbour-meteoswiss-it.ts
@@ -0,0 +1,314 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_CH">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About MeteoSwiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This is an unofficial client app for the national weather service&apos;s web interface.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright, Federal Office of Meteorology and Climatology MeteoSwiss.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Weather icons by Zeix.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>https://www.meteoschweiz.admin.ch/</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>License GPLv3+: GNU GPL version 3 or later.
+This is free software: you are free to change and redistribute it.There is NO WARRANTY, to the extent permitted by law.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DaySummaryItem</name>
+    <message>
+        <source>Today</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ForecastGraphItem</name>
+    <message>
+        <source>Temperature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Precipitation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wind</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ForecastItem</name>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1: %2</source>
+        <comment>time (1) with weather description (2)</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sun Times</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dawn</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>morning civil twilight starts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sunrise</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>top edge of the sun appears on the horizon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Golden Hour End</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>morning golden hour (soft light, best time for photography) ends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Solar Noon</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sun is in the highest position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Golden Hour</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>evening golden hour (soft light, best time for photography) starts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sun disappears below the horizon, evening civil twilight starts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Night</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>dark enough for astronomical observations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nadir</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>darkest moment of the night, sun is in the lowest position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>status: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>now: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ForecastPage</name>
+    <message>
+        <source>Reload Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to load data!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocationSearchPage</name>
+    <message>
+        <source>Bootstrap debug locations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OverviewListDelegate</name>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move to top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add a location first</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to add items</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SunTimesItem</name>
+    <message>
+        <source>%1: %2</source>
+        <comment>sun time title (1) with description (2)</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TablePage</name>
+    <message>
+        <source>Detailed Forecast</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temp.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Precip.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wind</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>harbour-meteoswiss</name>
+    <message>
+        <source>d MMM yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>hh&apos;:&apos;mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ddd d MMM yyyy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
