9f78f16ff19f3bc7473c50112aae47b794549ad2
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-12-05 00:58:49 +0800

    Create harbour-meteoswiss-zh_CN.ts

---
+++ translations/harbour-meteoswiss-zh_CN.ts
@@ -0,0 +1,315 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About MeteoSwiss</source>
+        <translation>关于 MeteoSwiss</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>作者</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>数据</translation>
+    </message>
+    <message>
+        <source>Copyright, Federal Office of Meteorology and Climatology MeteoSwiss.</source>
+        <translation>版权所有，联邦气象与气候办公室，MeteoSwiss.</translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation>网站</translation>
+    </message>
+    <message>
+        <source>https://www.meteoschweiz.admin.ch/</source>
+        <translation>https://www.meteoswiss.admin.ch/</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>版本 %1</translation>
+    </message>
+    <message>
+        <source>This is an unofficial client app for the national weather service&apos;s web interface.</source>
+        <translation>这是一个非官方国家气象服务网站客户端。</translation>
+    </message>
+    <message>
+        <source>Weather icons by Zeix.</source>
+        <translation>天气图标作者为 Zeix 。</translation>
+    </message>
+    <message>
+        <source>License GPLv3+: GNU GPL version 3 or later.
+This is free software: you are free to change and redistribute it.There is NO WARRANTY, to the extent permitted by law.</source>
+        <translation>许可协议为 GPLv3+: GNU GPL 版本3 及以上。
+这是一个自由软件，因此你可以自由修改及分发。在法律许可范围内没有任何限制。</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>源代码位于 GitHub</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation>MeteoSwiss</translation>
+    </message>
+</context>
+<context>
+    <name>DaySummaryItem</name>
+    <message>
+        <source>Today</source>
+        <translation>今日</translation>
+    </message>
+</context>
+<context>
+    <name>ForecastGraphItem</name>
+    <message>
+        <source>Temperature</source>
+        <translation>温度</translation>
+    </message>
+    <message>
+        <source>Precipitation</source>
+        <translation>降水</translation>
+    </message>
+    <message>
+        <source>Wind</source>
+        <translation>有风</translation>
+    </message>
+</context>
+<context>
+    <name>ForecastItem</name>
+    <message>
+        <source>Loading...</source>
+        <translation>加载中...</translation>
+    </message>
+    <message>
+        <source>Failed...</source>
+        <translation>加载失败...</translation>
+    </message>
+    <message>
+        <source>status: </source>
+        <translation>状态: </translation>
+    </message>
+    <message>
+        <source>now: </source>
+        <translation>当前: </translation>
+    </message>
+    <message>
+        <source>unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <source>%1: %2</source>
+        <comment>time (1) with weather description (2)</comment>
+        <translation>%1: %2</translation>
+    </message>
+    <message>
+        <source>Sun Times</source>
+        <translation>日出时间</translation>
+    </message>
+    <message>
+        <source>top edge of the sun appears on the horizon</source>
+        <translation>太阳露出地平线</translation>
+    </message>
+    <message>
+        <source>morning golden hour (soft light, best time for photography) ends</source>
+        <translation>清晨的黄金时间（柔和的光线，最佳摄影时间）结束</translation>
+    </message>
+    <message>
+        <source>sun is in the highest position</source>
+        <translation>太阳位于最高点</translation>
+    </message>
+    <message>
+        <source>evening golden hour (soft light, best time for photography) starts</source>
+        <translation>夜晚黄金时间（柔和的光线，最佳摄影时间）开始</translation>
+    </message>
+    <message>
+        <source>sun disappears below the horizon, evening civil twilight starts</source>
+        <translation>太阳消失在地平线，夜幕降临</translation>
+    </message>
+    <message>
+        <source>dark enough for astronomical observations</source>
+        <translation>夜色足够黑暗适于天文观测</translation>
+    </message>
+    <message>
+        <source>darkest moment of the night, sun is in the lowest position</source>
+        <translation>夜晚最黑的时刻，太阳处于最低位置</translation>
+    </message>
+    <message>
+        <source>morning civil twilight starts</source>
+        <translation>东方微明</translation>
+    </message>
+    <message>
+        <source>Dawn</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>拂晓</translation>
+    </message>
+    <message>
+        <source>Sunrise</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>日出</translation>
+    </message>
+    <message>
+        <source>Golden Hour End</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>黄金时间结束</translation>
+    </message>
+    <message>
+        <source>Solar Noon</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>卓午</translation>
+    </message>
+    <message>
+        <source>Golden Hour</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>黄金时间</translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>日落</translation>
+    </message>
+    <message>
+        <source>Night</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>夜晚</translation>
+    </message>
+    <message>
+        <source>Nadir</source>
+        <comment>use &apos;|&apos; to separate alternative strings of different length</comment>
+        <translation>太阳最低点</translation>
+    </message>
+</context>
+<context>
+    <name>ForecastPage</name>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation>MeteoSwiss</translation>
+    </message>
+    <message>
+        <source>Failed to load data!</source>
+        <translation>加载数据失败！</translation>
+    </message>
+    <message>
+        <source>Reload Data</source>
+        <translation>重新加载数据</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+</context>
+<context>
+    <name>LocationSearchPage</name>
+    <message>
+        <source>Add Location</source>
+        <translation>添加位置</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <source>Bootstrap debug locations</source>
+        <translation>调整位置</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewListDelegate</name>
+    <message>
+        <source>Remove</source>
+        <translation>移除</translation>
+    </message>
+    <message>
+        <source>Move to top</source>
+        <translation>移到顶部</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>删除中</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <source>MeteoSwiss</source>
+        <translation>MeteoSwiss</translation>
+    </message>
+    <message>
+        <source>Add location</source>
+        <translation>添加位置</translation>
+    </message>
+    <message>
+        <source>Add a location first</source>
+        <translation>先添加当地位置</translation>
+    </message>
+    <message>
+        <source>Pull down to add items</source>
+        <translation>下拉以添加项目</translation>
+    </message>
+</context>
+<context>
+    <name>SunTimesItem</name>
+    <message>
+        <source>%1: %2</source>
+        <comment>sun time title (1) with description (2)</comment>
+        <translation>%1: %2</translation>
+    </message>
+</context>
+<context>
+    <name>TablePage</name>
+    <message>
+        <source>Detailed Forecast</source>
+        <translation>详细预报</translation>
+    </message>
+    <message>
+        <source>Hour</source>
+        <translation>小时</translation>
+    </message>
+    <message>
+        <source>Temp.</source>
+        <translation>温度</translation>
+    </message>
+    <message>
+        <source>Precip.</source>
+        <translation>降水</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <source>Wind</source>
+        <translation>风</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-meteoswiss</name>
+    <message>
+        <source>d MMM yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <translation>d MMM yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</translation>
+    </message>
+    <message>
+        <source>ddd d MMM yyyy</source>
+        <translation>ddd d MMM yyyy</translation>
+    </message>
+    <message>
+        <source>hh&apos;:&apos;mm</source>
+        <translation>hh&apos;:&apos;mm</translation>
+    </message>
+</context>
+</TS>
