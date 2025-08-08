5d63248822f982d10b9ebd0a22843d911c65ee61
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-08-08 21:08:13 +0200

    Update Opal modules

--- qml/modules/Opal/SupportMe/private/LinkHandler/LinkHandler.js
+++ qml/modules/Opal/SupportMe/private/LinkHandler/LinkHandler.js
@@ -2,5 +2,4 @@
 //@ https://github.com/Pretty-SFOS/opal-linkhandler
 //@ SPDX-FileCopyrightText: 2020-2023 Mirian Margiani
 //@ SPDX-License-Identifier: GPL-3.0-or-later
-function openOrCopyUrl(externalUrl,title){pageStack.push(Qt.resolvedUrl("private/ExternalUrlPage.qml"),{"externalUrl":externalUrl,"title":!!title?title:""})
-}
+function openOrCopyUrl(externalUrl,title){pageStack.push(Qt.resolvedUrl("private/ExternalUrlPage.qml"),{"externalUrl":externalUrl,"title":!!title?title:""});};
